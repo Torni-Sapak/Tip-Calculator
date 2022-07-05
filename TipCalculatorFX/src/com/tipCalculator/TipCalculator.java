c58038c47415af60cc4fce3c23c97301fe4889e7
Torni-Sapak <51757527+Torni-Sapak@users.noreply.github.com>
2022-07-05 14:43:04 +0000

    simple intrest calculator in java

---
+++ TipCalculatorFX/src/com/tipCalculator/TipCalculator.java
@@ -0,0 +1,27 @@
+package com.tipCalculator;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TipCalculator extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+        Scene scene = new Scene(root); // attach scene graph to scene
+        stage.setTitle("Tip Calculator"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
+    }
+
+}
